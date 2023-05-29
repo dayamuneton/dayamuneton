7ccf5bd3ee23365482d9747f9e6c9bb34b9a551e
agredo-alejo <agredoalejo@gmail.com>
2023-05-29 14:27:42 -0400

    commented apiConvertions

--- integrations/convertions/apiConvertions.ts
+++ integrations/convertions/apiConvertions.ts
@@ -1,18 +1,19 @@
 export const apiConvertions = async (reqPayload: any) => {
-   if (process.env.NODE_ENV === "development") return {};
+   // if (process.env.NODE_ENV === "development") return {};
+   return {};
 
-   const response = await fetch(
-      `${process.env.NEXT_PUBLIC_MY_DOMAIN}/api/convertions`,
-      {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/json",
-         },
-         body: JSON.stringify({
-            reqPayload,
-         }),
-      }
-   );
+   // const response = await fetch(
+   //    `${process.env.NEXT_PUBLIC_MY_DOMAIN}/api/convertions`,
+   //    {
+   //       method: "POST",
+   //       headers: {
+   //          "Content-Type": "application/json",
+   //       },
+   //       body: JSON.stringify({
+   //          reqPayload,
+   //       }),
+   //    }
+   // );
 
-   return await response.json();
+   // return await response.json();
 };
