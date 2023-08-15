cc7d17251f846b6d1c802f295c7888ef56522d18
agredo-alejo <agredoalejo@gmail.com>
2023-08-15 11:57:50 -0400

    update shop images

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -36,57 +36,59 @@ export default function Home() {
          <div className="flex flex-col gap-3 mt-4 sm:flex-row">
             <Link
                href="/shop/heads"
-               className="relative hover:underline drop-shadow-lg group"
+               className="relative hover:underline drop-shadow-lg group bg-gradient-to-r to-[#5bdde5] from-[#034daf] rounded-3xl overflow-hidden"
             >
                <SquareImageHover
-                  src="/ShopArtHeads.jpg"
+                  src="/newshopartcollection.png"
                   alt="Shop My Art Heads"
                   width="w-[80vw] sm:w-[27vw] max-w-[20rem] rounded-[2rem]"
                   scale="group-hover:scale-[1.05]"
                />
                <span className="text-xl text-black whitespace-nowrap max-w-[25vw] font-bold uppercase absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center cursor-pointer">
-                  <p>HEADS</p>
+                  <p>FORGOTTEN BODIES</p>
+                  {/* <p className="text-sm">ART COLLECTION</p> */}
                </span>
             </Link>
             <Link
-               href="/shop/bodies"
-               className="relative hover:underline drop-shadow-lg group"
+               href="/shop/memorials"
+               className="relative hover:underline drop-shadow-lg group bg-gradient-to-r to-[#5bdde5] from-[#034daf] rounded-3xl overflow-hidden"
             >
                <SquareImageHover
-                  src="/ShopArtBodies.png"
+                  src="/newshopcelebrating.png"
                   alt="Shop My Art Bodies"
                   width="w-[80vw] sm:w-[27vw] max-w-[20rem] rounded-[2rem]"
                   scale="group-hover:scale-[1.05]"
                />
                <span className="text-xl text-black whitespace-nowrap max-w-[25vw] font-bold uppercase absolute top-0 left-1/2 -translate-x-1/2 translate-y-1/2 text-center cursor-pointer">
                   {/* <p>Shop</p> */}
-                  <p>Forgoten</p>
-                  <p>Bodies</p>
+                  <p>CELEBRATING OUR </p>
+                  <p>MUSICAL HERITAGE</p>
+                  {/* <p>ART COLLECTION</p> */}
                </span>
             </Link>
             <Link
                href="/shop/abstracts"
-               className="relative group drop-shadow-lg"
+               className="relative group drop-shadow-lg bg-gradient-to-r to-[#5bdde5] from-[#034daf] rounded-3xl overflow-hidden"
                // target="_blank"
             >
                <SquareImageHover
-                  src="/StoriesBehindTheSongs.png"
+                  src="/newshopdayashop.png"
                   alt="Shop My Art Abstracts"
                   width="w-[80vw] sm:w-[27vw] max-w-[20rem] rounded-[2rem]"
                   scale="group-hover:scale-[1.05]"
                />
                <span className="text-xl text-black whitespace-nowrap max-w-[25vw] font-bold uppercase absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center cursor-pointer">
                   {/* <p></p> */}
-                  <p>Abstracts</p>
+                  <p>{`DAYA'S SHOP`}</p>
                </span>
             </Link>
          </div>
-         <Link
+         {/* <Link
             href="/shop/memorials"
             className=" mt-4 py-3 text-base text-black bg-gradient-to-r from-[#5bdde5] to-[#034daf] rounded-full w-[90vw] max-w-xl drop-shadow-md uppercase font-bold px-4 sm:px-20 text-center"
          >
             Memorials
-         </Link>
+         </Link> */}
          <Link
             href="/shop/custom"
             className="mb-20 mt-4 py-3 text-base text-black bg-gradient-to-r from-[#5bdde5] to-[#034daf] rounded-full w-[90vw] max-w-xl drop-shadow-md uppercase font-bold px-4 sm:px-20 animate-wiggle text-center"
